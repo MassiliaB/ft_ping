1f6d5c9b61d234b69df7c7b0d74e9a1028837966
Massilia <massblri@gmail.com>
2024-06-04 13:00:20 +0200

    cleaning

---
+++ includes/ft_ping.h
@@ -0,0 +1,32 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <netpacket/packet.h>
+#include <errno.h>
+//#include <linux/time.h>
+//#include <ip.h>
+
+#define PING_PKT_S 64 // ping packet size
+#define PORT_NO 0 // Automatic port number
+#define PING_SLEEP_RATE 1000000 // Automatic port number
+#define RECV_TIMEOUT 1 // Timeout delay for receiving packets in seconds
+#define NI_MAXHOST      1025
+#define NI_MAXSERV      32
+
+#define DATALEN         56
+#define IPMAXLEN        60
+#define ICMPMAXLEN      76
+
+char    *reverse_dns_lookup(char *ip_addr);
+char    *dns_lookup(char *hostname, struct sockaddr_in *addr_connexion);
+void    send_ping(int raw_sockfd, struct sockaddr_in *addr_con, char *ping_domain, char *ping_ip, char *rev_host);
+void    intHandler();
---
+++ src/ft_ping.c
@@ -0,0 +1,41 @@
+#include "../includes/ft_ping.h"
+
+int open_rawsock()
+{
+    int sockfd; //Raw socket - if you use IPPROTO_ICMP, then kernel will fill in the correct ICMP header checksum, if IPPROTO_RAW, then it wont
+    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
+    if (sockfd == -1) {
+        printf("Error creating socket : %s\n", strerror(errno));
+        return -1;
+    }
+    return sockfd;
+}
+
+int main(int ac, char **av)
+{
+    int     sockfd;
+    struct  sockaddr_in dest_addr; // ? 
+  //  char    *options;
+    char    *ip_addr;
+    char    *reverse_hostname; // To convert ip addr to hostname
+
+    if (getuid() != 0){
+        printf("This program requires root privileges!\n");
+        return 0;
+    }
+    if (ac < 2 || ac > 3){
+        printf("usage: ping [-v][-?] ‹Hostname or IP>\n");
+        return 0;
+    }
+    if (!(ip_addr = dns_lookup(av[1], &dest_addr))){
+        printf("DNS lookup failed: could not resolve hostname !\n");
+        return 0;
+    }
+    reverse_hostname = reverse_dns_lookup(ip_addr); 
+    if ((sockfd = open_rawsock()) < 0)
+        return -1;
+    signal(SIGINT, intHandler); // catching interrupt
+    send_ping(sockfd, &dest_addr, reverse_hostname, ip_addr, av[1]);
+    return 0;
+}
